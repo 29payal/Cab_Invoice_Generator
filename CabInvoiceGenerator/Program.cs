bef785f7d7407f6126253065265d963dec15762e
Payal Sharma <https://github.com/29payal>
2022-10-06 19:47:59 +0530

    [Payal] Cab_Invoice_Generator

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double Fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine("Total Fare is : " +Fare);
+            Console.ReadLine();
+        }
+    }
+}
